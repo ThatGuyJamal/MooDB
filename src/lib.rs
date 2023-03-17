c335b2a5e9ea73e9e3b7653d90083c9f518c9c4f
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2023-03-17 13:19:51 -0400

    Updates

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,14 @@
 #![allow(unused_variables)]
 #![allow(dead_code)]
 
+mod types;
+
 use std::fs::{File, OpenOptions};
 use std::io::{Read, Seek, SeekFrom, Write};
 use std::path::PathBuf;
 
-use types::{MooError, StorageType, Record};
-
-mod types;
+use serde_json::json;
+use types::{MooError, MooRecord, MooResult, Record, StorageType, MooErrorCodes};
 
 pub struct MooDB {
     // database file path
@@ -16,56 +17,165 @@ pub struct MooDB {
     // database file
     pub file: File,
     // database storage type
-    pub storage_option: StorageType,
+    pub storage_type: StorageType,
+    // database records
+    pub records: MooRecord,
 }
 
 impl MooDB {
     /// Creates a new MooDB instance.
     ///
     /// If `custom_file_name` is `None`, the database
-    /// will be created in the current directory with the name `moodb.json`.
-    pub fn new(custom_file_name: Option<String>, custom_storage_option: Option<StorageType>) -> Result<Self, MooError> {
+    /// will be created in the current directory with the name `moo.json`.
+    pub fn new(
+        custom_file_name: Option<String>,
+        custom_storage_type: Option<StorageType>,
+    ) -> MooResult<Self> {
         let path = match custom_file_name {
             Some(name) => PathBuf::from(name),
-            None => PathBuf::from("moodb.json"),
+            None => PathBuf::from("moo.json"),
         };
 
-        let storage_option = match custom_storage_option {
+        let storage_type = match custom_storage_type {
             Some(option) => option,
             None => StorageType::Json,
         };
 
-        let file = OpenOptions::new()
-            .create(true)
+        let file = match OpenOptions::new()
             .read(true)
             .write(true)
+            .create(true)
             .open(&path)
-            .map_err(|e| MooError {
-                code: types::MooErrorCodes::Error,
-                message: format!("{}", e),
-            })?;
+        {
+            Ok(file) => file,
+            Err(e) => {
+                return Err(MooError {
+                    code: MooErrorCodes::Error,
+                    message: e.to_string(),
+                })
+            }
+        };
 
-        Ok(Self { path, file, storage_option })
+        Ok(Self {
+            path,
+            file,
+            storage_type,
+            records: vec![],
+        })
     }
 
     /// Inserts a record into the database.
     ///
     /// If the record already exists, it will be overwritten.
-    pub fn insert(&mut self, record: &Record) -> Result<(), MooError> {
+    pub fn insert(&mut self, record: &Record) -> MooResult<()> {
+        let json = json!(record);
+        //
         todo!()
     }
 
     /// Selects a record from the database.
     ///
     /// If the record does not exist, `None` will be returned.
-    pub fn select(&mut self, id: u32) -> Result<Option<Record>, MooError> {
+    pub fn select(&mut self, id: u32) -> MooResult<Record> {
         todo!()
+    }
+
+    /// Deletes a record from the database.
+    pub fn delete(&mut self, id: u32) -> MooResult<bool> {
+        todo!()
+    }
+
+    /// Updates a record in the database.
+    pub fn update(&mut self, id: String, record: &Record) -> MooResult<()> {
+        todo!()
+    }
+
+    /// Returns the number of records in the database.
+    pub fn count(&mut self) -> MooResult<usize> {
+        todo!()
+    }
+
+    /// Checks if a record exists in the database.
+    pub fn exists(&mut self, id: u32) -> MooResult<bool> {
+        todo!()
+    }
+
+    /// Creates or reads the database file.
+    fn create_or_read_file(&mut self) -> MooResult<File> {
+        let file = match OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(&self.path)
+        {
+            Ok(file) => file,
+            Err(e) => {
+                return Err(MooError {
+                    code: MooErrorCodes::Error,
+                    message: e.to_string(),
+                })
+            }
+        };
+
+        Ok(file)
     }
 
     /// Reads the entire database file into memory.
     ///
     /// This is a helper function that is used by `insert` and `select`.
-    fn read_all(&mut self) -> Result<Vec<u8>, types::MooError> {
+    fn find_all_records(&mut self) -> MooResult<MooRecord> {
         todo!()
+    }
+
+    /// Converts a `Record` to a `String` based on the database's storage type.
+    /// 
+    /// This is a helper function that is used by `insert` and `select`.
+    fn record_to_string(&mut self, record: &Record) -> MooResult<String> {
+        return match serde_json::to_string(record) {
+            Ok(json) => Ok(json),
+            Err(e) => Err(MooError {
+                code: MooErrorCodes::Error,
+                message: e.to_string(),
+            }),
+        };
+    }
+
+    /// Converts a `String` to a `Record` based on the database's storage type.
+    /// 
+    /// This is a helper function that is used by `insert` and `select`.
+    fn string_to_record(s: String) -> MooResult<Record> {
+        return match serde_json::from_str(&s) {
+            Ok(record) => Ok(record),
+            Err(e) => Err(MooError {
+                code: MooErrorCodes::Error,
+                message: e.to_string(),
+            }),
+        };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use serde_json::json;
+    use super::*;
+
+    #[test]
+    fn test_record_vectors() {
+        let mut db = MooDB::new(None, None).unwrap();
+        
+        let data = vec!["a", "b", "c"];
+        
+        let raw = Record {
+            key: "test".to_string(),
+            value: data.into()
+        };
+
+        let json = db.record_to_string(&raw).unwrap();
+
+        let record = MooDB::string_to_record(json).unwrap();
+
+        assert_eq!(record.key, "test");
+        assert_eq!(record.value, json!(["a", "b", "c"]));
+        assert_eq!(record.value.as_array().unwrap().len(), 3);
     }
 }
--- src/types.rs
+++ src/types.rs
@@ -1,26 +1,35 @@
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
-/// A record in the database. 
-/// 
+/// Return Type for common db actions
+pub type MooResult<T> = Result<T, MooError>;
+
+pub type MooRecord = Vec<Record>;
+
+/// A record in the database.
+///
 /// This is a simple struct that contains an ID and a value.
-/// 
+///
 /// Later this will be able to be customized by the user using there own struct that
-/// implements the `Serialize` and `Deserialize` traits. It will contain and id and any 
+/// implements the `Serialize` and `Deserialize` traits. It will contain and id and any
 /// other data they want to store in a struct.
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
 pub struct Record {
-    id: u32,
-    value: String,
+    /// The Key of the record and how data will be accessed.
+    pub key: String,
+    /// The value of the record.
+    pub value: Value,
 }
 
 /// The storage type of the database.
-/// 
+///
 /// This is an enum that contains the different storage types that the database can use.
-/// 
+///
 /// The default storage type is `StorageType::Json`.
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub enum StorageType {
     Memory,
-    Json
+    Json,
 }
 
 /// The error types for the database.
@@ -29,12 +38,12 @@ pub enum MooErrorCodes {
     Debug,
     Warn,
     Error,
-    Fatal
+    Fatal,
 }
 
 /// The error struct for the database.
 #[derive(Debug)]
 pub struct MooError {
-    pub code: MooErrorCodes, 
-    pub message: String
+    pub code: MooErrorCodes,
+    pub message: String,
 }
